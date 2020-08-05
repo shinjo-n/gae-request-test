3ae2ed45dd5626ea66e5192a0fc2bc623a2aa9ee
N.Shinjo <shinjo@cloud-ace.jp>
2020-08-05 15:03:08 +0900

    add Method:jsonHandlerFunc
    json用メソッド jsonHandlerFunc を実装

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/gommon/log"
+	"io"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/", IndexHandler)
+
+	http.HandleFunc("/json", jsonHandlerFunc)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+
 	if err := r.ParseForm(); err != nil {
 		fmt.Println("error")
 	}
@@ -20,5 +28,41 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%v : %v\n", k, v)
 	}
 
+	w.Write([]byte("success"))
+}
+
+func jsonHandlerFunc(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if req.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	body := make([]byte, length)
+	length, err = req.Body.Read(body)
+	if err != nil && err != io.EOF {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var jsonBody map[string]interface{}
+	err = json.Unmarshal(body[:length], &jsonBody)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%v\n", jsonBody)
+
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("success"))
 }
--- main_test.go
+++ main_test.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestRoot(t *testing.T) {
+func TestIndex(t *testing.T) {
 	testserver := httptest.NewServer(http.HandlerFunc(IndexHandler))
 	defer testserver.Close()
 
 	res, err := http.Get(testserver.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Error("a response code is not 200")
+	}
+
+	if string(body) != "success" {
+		t.Error("a response is not success")
+	}
+}
+
+func TestJson(t *testing.T) {
+	testserver := httptest.NewServer(http.HandlerFunc(jsonHandlerFunc))
+	defer testserver.Close()
+
+	json := bytes.NewBuffer([]byte(`{"token":"test","device":"test2"}`))
+	res, err := http.Post(testserver.URL, "application/json", json)
 	if err != nil {
 		t.Error(err)
 	}
